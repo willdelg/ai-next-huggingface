87f7df987f8905b15e5536b651bc75f201138744
willdelg <57252327+willdelg@users.noreply.github.com>
2023-10-05 19:04:20 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,14 @@
-'use client';
+import React from 'react';
+import { Chat } from './components/Chat';
 
-import { useChat } from 'ai/react';
-
-export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
-
+function Page() {
   return (
-    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.length > 0
-        ? messages.map(m => (
-            <div key={m.id} className="whitespace-pre-wrap">
-              {m.role === 'user' ? 'User: ' : 'AI: '}
-              {m.content}
-            </div>
-          ))
-        : null}
-
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
-          value={input}
-          placeholder="Say something..."
-          onChange={handleInputChange}
-        />
-      </form>
+    <div>
+      {/* ... other components or content */}
+      <Chat />
+      {/* ... other components or content */}
     </div>
   );
 }
+
+export default Page;
